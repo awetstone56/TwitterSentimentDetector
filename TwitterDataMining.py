10042a6b4dff708817c37fc9db3b41609c6f9305
Barrett <awetstone56@gmail.com>
2017-11-30 01:55:36 -0500

    Added initial files
    Added twitter data access file and pre processing file

---
+++ TwitterDataMining.py
@@ -0,0 +1,33 @@
+import tweepy # pip install tweepy==3.5.0
+#import django # pip install Django
+import json
+import configparser
+#from django.utils.encoding import smart_str, smart_unicode
+from tweepy import OAuthHandler
+
+config = configparser.ConfigParser()
+config.read('TwitterSentimentDetector.ini')
+
+file = open("Tweets.json", "w") # call file what ever you need to
+# this method writes a tweet to a file
+def process_or_store(tweet):
+    file.write(json.dumps(tweet) + "\n")
+
+consumer_key = config['twitter.com']['consumer_key']
+consumer_secret = config['twitter.com']['consumer_secret']
+access_token = config['twitter.com']['access_token']
+access_secret = config['twitter.com']['access_secret']
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+# api is what you use to access anything from twitter
+api = tweepy.API(auth)
+
+# change the number of tweets it grabs by changing the number '25'
+for tweet in tweepy.Cursor(api.home_timeline).items(25):
+    # Process a single status
+    # if you're going to print any text use smart_str from django
+    # print smart_str(status._json)
+    process_or_store(tweet._json)
+
+file.close()
