e040171bfe9618ebc09ed832cc3dd630d1cd31b3
Barrett <awetstone56@gmail.com>
2017-12-05 01:09:01 -0500

    Added sentiment detection using lexicon

--- TweetPreProcessing.py
+++ TweetPreProcessing.py
@@ -2,6 +2,7 @@ import json
 import re
 import operator
 import configparser
+import math
 from collections import Counter
 from collections import defaultdict
 import nltk
@@ -51,20 +52,30 @@ def preprocess(s, lowercase=False):
     return tokens
 
 fname = config['DEFAULT']['tweet_file']
+
 # co-occurence matrix
 com = defaultdict(lambda : defaultdict(int))
+# document frequency of terms not including stop words
+count_stop_single = Counter()
+# document frequency of terms including stop words
+count_single = Counter()
+tweetCount = 0.0
 
 with open(fname, 'r') as f:
-    count_all = Counter()
     for line in f:
+        tweetCount = tweetCount + 1
         tweet = json.loads(line) # load it as Python dict
+
         # Create a list with all the terms
         terms_all = [term for term in preprocess(tweet['text'])]
+
         # Count terms only once, equivalent to Document Frequency
-        terms_single = set(terms_all)
+        count_single = set(terms_all)
+
         # Count hashtags only
-        terms_hash = [term for term in preprocess(tweet['text']) 
-              if term.startswith('#')]
+        #terms_hash = [term for term in preprocess(tweet['text']) 
+        #      if term.startswith('#')]
+
         # Count terms only (no hashtags, no mentions)
         terms_only = [term for term in preprocess(tweet['text']) 
               if term not in stop and
@@ -74,10 +85,10 @@ with open(fname, 'r') as f:
               # we pass a list of inputs
         terms_stop = [term for term in preprocess(tweet['text']) if term not in stop]
         # Update the Counter
-        count_all.update(terms_stop)
+        count_stop_single.update(terms_stop)
         # The bigrams() function from NLTK will take a list of tokens 
         # and produce a list of tuples using adjacent tokens
-        terms_bigram = bigrams(terms_stop) # have to cast bigram as a list
+        #terms_bigram = bigrams(terms_stop) # have to cast bigram as a list
         #print(list(terms_bigram)) 
 
         # Build co-occurrence matrix
@@ -87,4 +98,51 @@ with open(fname, 'r') as f:
                 if w1 != w2:
                     com[w1][w2] += 1
                     
-    print(count_all.most_common(5))
+    #print(count_stop_single.most_common(5))
+
+file.close(f)
+
+# p_t is the probability of the term occurring in a document
+p_t = {}
+# p_t_com is the probability of two terms occurring together in a document
+p_t_com = defaultdict(lambda : defaultdict(int))
+
+#
+for term, n in count_stop_single.items():
+    p_t[term] = n / tweetCount
+    for t2 in com[term]:
+        p_t_com[term][t2] = com[term][t2] / tweetCount
+
+fname = config['DEFAULT']['positive_words']
+positive_vocab = []
+with open(fname, 'r') as f:
+    positive_vocab = f.read().splitlines()
+file.close(f)
+
+fname = config['DEFAULT']['negative_words']
+negative_vocab = []
+with open(fname, 'r') as f:
+    negative_vocab = f.read().splitlines()
+
+# pmi is the Pointwise Mutual Information
+# or the closeness of a word to terms like good or bad
+pmi = defaultdict(lambda : defaultdict(int))
+for t1 in p_t:
+    for t2 in com[t1]:
+        denom = p_t[t1] * p_t[t2]
+        pmi[t1][t2] = math.log((p_t_com[t1][t2] / denom), 2)
+ 
+semantic_orientation = {}
+for term, n in p_t.items():
+    positive_assoc = sum(pmi[term][tx] for tx in positive_vocab)
+    negative_assoc = sum(pmi[term][tx] for tx in negative_vocab)
+    semantic_orientation[term] = positive_assoc - negative_assoc
+
+semantic_sorted = sorted(semantic_orientation.items(), 
+                         key=operator.itemgetter(1), 
+                         reverse=True)
+top_pos = semantic_sorted[:10]
+top_neg = semantic_sorted[-10:]
+ 
+print(top_pos)
+print(top_neg)
